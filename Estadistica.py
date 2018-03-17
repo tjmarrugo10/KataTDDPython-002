d2cf105af519c49582a6aa5ce61384b728a6f5f3
camendoza94 <ca.mendoza968@uniandes.edu.co>
2018-03-17 12:06:45 -0500

    Paso 3 - Se modifica el código para cuando se tienen dos números (verde)

--- Estadistica.py
+++ Estadistica.py
@@ -2,6 +2,8 @@ class Estadistica:
     def stats(self, cadena):
         if cadena == "":
             return [0]
+        elif "," in cadena:
+            return [2]
         else:
             return [1]
 
