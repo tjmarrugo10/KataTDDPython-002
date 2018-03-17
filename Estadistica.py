9bbbf5db4dab891721fa24b0175e29987b81374d
tjmarrugo10 <tj.marrugo10@uniandes.edu.co>
2018-03-17 13:02:06 -0500

    Paso 12-13: Iteración 3- Se modifica el código para cuando la cadena tiene más de un número, mínimo y maximo (12-verde. 13-rojo y verde)

--- Estadistica.py
+++ Estadistica.py
@@ -4,7 +4,7 @@ class Estadistica:
             return [0, 0, 0]
         elif "," in cadena:
             numeros = cadena.split(",")
-            return [len(numeros), int(min(numeros))]
+            return [len(numeros), int(min(numeros)),int(max(numeros))]
         else:
             return [int(cadena), int(cadena), int(cadena)]
 
--- test_estadistica.py
+++ test_estadistica.py
@@ -11,10 +11,10 @@ class TestEstadistica(TestCase):
         self.assertEqual(Estadistica().stats("1"), [1, 1, 1], "Un numero")
 
     def test_stats_dosnumeros(self):
-        self.assertEqual(Estadistica().stats("1,2"), [2, 1], "Dos numeros")
+        self.assertEqual(Estadistica().stats("1,2"), [2, 1,2], "Dos numeros")
 
     def test_stats_nnumeros(self):
-        self.assertEqual(Estadistica().stats("1,2,3,4"), [4, 1], "Multiples numeros")
+        self.assertEqual(Estadistica().stats("1,2,3,4"), [4, 1,4], "Multiples numeros")
 
     def test_stats_cadenavacia_minimo(self):
         self.assertEqual(Estadistica().stats(""), [0, 0, 0], "Cadena vacia y minimo")
@@ -23,7 +23,7 @@ class TestEstadistica(TestCase):
         self.assertEqual(Estadistica().stats("1"), [1, 1, 1], "Un numero y minimo")
 
     def test_stats_dosnumeros_minimo(self):
-        self.assertEqual(Estadistica().stats("1,2"), [2, 1], "Dos numeros y minimo")
+        self.assertEqual(Estadistica().stats("1,2"), [2, 1,2], "Dos numeros y minimo")
 
     def test_stats_cadenavacia_minimo_maximo(self):
         self.assertEqual(Estadistica().stats(""), [0, 0, 0], "Cadena vacia, minimo,maximo")
