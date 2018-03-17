eb4a20b95b1fc6a94026f7df446c8d475403ec9b
camendoza94 <ca.mendoza968@uniandes.edu.co>
2018-03-17 12:36:57 -0500

    Paso 7-8: Iteración 2- Se modifica el código para cuando la cadena tiene más de un número y mínimo (7-verde. 8-rojo y verde)

--- Estadistica.py
+++ Estadistica.py
@@ -4,7 +4,7 @@ class Estadistica:
             return [0, 0]
         elif "," in cadena:
             numeros = cadena.split(",")
-            return [len(numeros)]
+            return [len(numeros), int(min(numeros))]
         else:
-            return [1,1]
+            return [1, 1]
 
--- test_estadistica.py
+++ test_estadistica.py
@@ -8,13 +8,13 @@ class TestEstadistica(TestCase):
         self.assertEqual(Estadistica().stats(""), [0, 0], "Cadena vacia")
 
     def test_stats_unacadena(self):
-        self.assertEqual(Estadistica().stats("1"), [1,1], "Un numero")
+        self.assertEqual(Estadistica().stats("1"), [1, 1], "Un numero")
 
     def test_stats_dosnumeros(self):
-        self.assertEqual(Estadistica().stats("1,2"), [2], "Dos numeros")
+        self.assertEqual(Estadistica().stats("1,2"), [2, 1], "Dos numeros")
 
     def test_stats_nnumeros(self):
-        self.assertEqual(Estadistica().stats("1,2,3,4"), [4], "Multiples numeros")
+        self.assertEqual(Estadistica().stats("1,2,3,4"), [4, 1], "Multiples numeros")
 
     def test_stats_cadenavacia_minimo(self):
         self.assertEqual(Estadistica().stats(""), [0, 0], "Cadena vacia y minimo")
