97065f47e822e1c8981938705e1c8473c5a4b47c
camendoza94 <ca.mendoza968@uniandes.edu.co>
2018-03-17 13:23:11 -0500

    Paso 15 Iteración 4 - Se agrega prueba para dos numeros y promedio (rojo)

--- test_estadistica.py
+++ test_estadistica.py
@@ -8,7 +8,7 @@ class TestEstadistica(TestCase):
         self.assertEqual(Estadistica().stats(""), [0, 0, 0, 0], "Cadena vacia")
 
     def test_stats_unacadena(self):
-        self.assertEqual(Estadistica().stats("1"), [1, 1, 1,1], "Un numero")
+        self.assertEqual(Estadistica().stats("1"), [1, 1, 1, 1], "Un numero")
 
     def test_stats_dosnumeros(self):
         self.assertEqual(Estadistica().stats("1,2"), [2, 1, 2], "Dos numeros")
@@ -20,7 +20,7 @@ class TestEstadistica(TestCase):
         self.assertEqual(Estadistica().stats(""), [0, 0, 0, 0], "Cadena vacia y minimo")
 
     def test_stats_unacadena_minimo(self):
-        self.assertEqual(Estadistica().stats("1"), [1, 1, 1,1], "Un numero y minimo")
+        self.assertEqual(Estadistica().stats("1"), [1, 1, 1, 1], "Un numero y minimo")
 
     def test_stats_dosnumeros_minimo(self):
         self.assertEqual(Estadistica().stats("1,2"), [2, 1, 2], "Dos numeros y minimo")
@@ -29,7 +29,7 @@ class TestEstadistica(TestCase):
         self.assertEqual(Estadistica().stats(""), [0, 0, 0, 0], "Cadena vacia, minimo,maximo")
 
     def test_stats_unnumero_minimo_maximo(self):
-        self.assertEqual(Estadistica().stats("1"), [1, 1, 1,1], "Un numero, minimo, maximo")
+        self.assertEqual(Estadistica().stats("1"), [1, 1, 1, 1], "Un numero, minimo, maximo")
 
     def test_stats_dosnumeros_minimo_maximo(self):
         self.assertEqual(Estadistica().stats("1,2"), [2, 1, 2], "Dos numeros,minimo,maximo")
@@ -38,5 +38,8 @@ class TestEstadistica(TestCase):
         self.assertEqual(Estadistica().stats(""), [0, 0, 0, 0], "Cadena vacia y promedio")
 
     def test_stats_unnumero_promedio(self):
-        self.assertEqual(Estadistica().stats("1"),[1,1,1,1],"Un numero, minimo, maximo, promedio")
+        self.assertEqual(Estadistica().stats("1"), [1, 1, 1, 1], "Un numero, minimo, maximo, promedio")
+
+    def test_stats_dosnumeros_promedio(self):
+        self.assertEqual(Estadistica().stats("1,2"), [2, 1, 2, 1.5], "Dos numeros, minimo, maximo y promedio")
 
