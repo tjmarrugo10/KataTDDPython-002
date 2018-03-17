d7f5b852d481c2ac1c9d45d02effb9bb2df0f60e
camendoza94 <ca.mendoza968@uniandes.edu.co>
2018-03-17 12:23:49 -0500

    Paso 5: Iteración 2 - Se modifica el código para cuando la cadena es vacía (verde)

--- Estadistica.py
+++ Estadistica.py
@@ -1,9 +1,9 @@
 class Estadistica:
     def stats(self, cadena):
         if cadena == "":
-            return [0]
+            return [0, 0]
         elif "," in cadena:
-            numeros=cadena.split(",")
+            numeros = cadena.split(",")
             return [len(numeros)]
         else:
             return [1]
--- test_estadistica.py
+++ test_estadistica.py
@@ -5,7 +5,7 @@ from Estadistica import Estadistica
 
 class TestEstadistica(TestCase):
     def test_stats(self):
-        self.assertEqual(Estadistica().stats(""), [0], "Cadena vacia")
+        self.assertEqual(Estadistica().stats(""), [0, 0], "Cadena vacia")
 
     def test_stats_unacadena(self):
         self.assertEqual(Estadistica().stats("1"), [1], "Un numero")
